71ad0dae5b9b066856b9b9278d2f82fdd5d86485
Dongjue Xie <x5176518@gmail.com>
2023-09-26 12:20:14 -0400

    Number display adapter is not working yet but it is at the end of class

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,27 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter : RecyclerView.Adapter<> /* Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+    private val dataSet = mutableListOf<String>()
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
     //Step 3b: Complete function definitions for adapter
 
